d051cfa3fa7267ce9549a41cfa6b04af98db2a16
qiansc <van3008@163.com>
2020-05-28 19:14:55 +0800

    feat: add ts compile

---
+++ src/about.ts
@@ -0,0 +1,8 @@
+// import sketch from 'sketch'
+// documentation: https://developer.sketchapp.com/reference/api/
+
+export default function(context: Sketch.Context) {
+    context.document.showMessage("Welcome to 🙌");
+    // sketch.getSelectedDocument()
+  }
+
