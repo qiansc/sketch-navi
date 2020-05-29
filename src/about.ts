8fd261ea9dc15a81c16cde263c13738463700e3d
qiansc <van3008@163.com>
2020-05-29 16:32:04 +0800

    fix: compile ts file

--- src/about.ts
+++ src/about.ts
@@ -1,8 +1,24 @@
-// import sketch from 'sketch'
-// documentation: https://developer.sketchapp.com/reference/api/
+var sketch: Sketch = require('sketch');
+// // Or, require without UI, Settings, DataSupplier, Async,
+// var sketchDom: SketchDom = require('sketch/dom');
+// // and require others according to your needs
+// var async: Async = require('sketch/async');
+// var DataSupplier: DataSupplier = require('sketch/data-supplier');
+// var UI: UI = require('sketch/ui');
+// var Settings: Settings = require('sketch/settings');
 
-export default function(context: Sketch.Context) {
-    context.document.showMessage("Welcome to 🙌");
-    // sketch.getSelectedDocument()
+// var document: Document = sketch.getSelectedDocument();
+// var page: Page = document.selectedPage;
+
+
+export default function() {
+  const doc = sketch.getSelectedDocument()
+  const selectedLayers = doc.selectedLayers
+  const selectedCount = selectedLayers.length
+
+  if (selectedCount === 0) {
+    sketch.UI.message('No layers are selected.')
+  } else {
+    sketch.UI.message(`${selectedCount} layers selected.`)
   }
-
+}
--- webpack.skpm.config.js
+++ webpack.skpm.config.js
@@ -3,21 +3,20 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = function (config, isPluginCommand) {
   const isProduction = process.env.NODE_ENV == "production";
-  
+
   config.module.rules.push({
     test: /\.tsx?$/,
     exclude: /node_modules/,
     loader: 'ts-loader'
   });
-  
+
   if (!config.resolve) {
     config.resolve = {
       extensions: []
     };
   }
-  
+
   config.resolve.extensions = [...config.resolve.extensions, ".ts", ".tsx"];
-  
   // transformations for production (publish)
   if (isProduction) {
     config.mode = "production";
