cbae0e6b4d0a90362aa383ddf30617340ef96755
qiansc <van3008@163.com>
2020-06-18 12:56:59 +0800

    chore: recode panel

--- src/core/panel-factory.ts
+++ src/core/panel-factory.ts
@@ -5,36 +5,46 @@ export class PanelFactory{
     constructor(private documentID: string) {
 
     }
+
     regist(key: string, panelClass: typeof Panel) {
         this.map.set(panelClass, key);
     }
+
     hasPanel(panelClass: typeof Panel) {
         const key = this.map.get(panelClass);
         const id = this.getId(key);
         const threadDictionary = NSThread.mainThread().threadDictionary();
         const view = threadDictionary[id];
         if (view) {
-            const panel = new Panel(id);
+            const panel = new Panel(
+                id,
+                this.getIDPrefix(key)
+            );
             panel.setView(view);
             return panel;
         } else {
             return;
         }
     }
+
     createPanel(panelClass: typeof Panel) {
         const key = this.map.get(panelClass);
-        const panel = new Panel(this.getId(key));
+        const panel = new Panel(
+            this.getId(key),
+            this.getIDPrefix(key)
+        );
         panel.createView();
-        const threadDictionary = NSThread.mainThread().threadDictionary();
-        threadDictionary[panel.id] = panel.view;
         return panel;
     }
 
     getIDPrefix(key: string) {
         return this.documentID ? `sketch-plugin-${key}-${this.documentID}` : `sketch-plugin-${key}`;
     }
+
     getId(key: string){
         const IDPrefix = this.getIDPrefix(key);
         return `${IDPrefix}-panel`;
     }
+
+
 }
--- src/core/panel.ts
+++ src/core/panel.ts
@@ -1,8 +1,11 @@
 export class Panel {
     public view: any;
-    constructor(public id: string) {
-
-    }
+    constructor(
+        /** view唯一标识 */
+        public id: string,
+        /** 其他view前缀 */
+        public prefix: string) {}
+    /** 创建view */
     public createView() {
         this.view = NSStackView.alloc().initWithFrame(NSMakeRect(0, 0, 40, 400));
 
@@ -12,16 +15,79 @@ export class Panel {
         this.view.setFlipped(true);
         this.view.setBackgroundColor(NSColor.windowBackgroundColor());
         this.view.orientation = 1
-        // insertSidePanel(context, toolbar, SidePanelIdentifier)
+        const threadDictionary = NSThread.mainThread().threadDictionary();
+        threadDictionary[this.id] = this.view;
+        COScript.currentCOScript().setShouldKeepAround(true);
     }
+
+    /** 设置已经存在的view */
     public setView(view: any) {
         this.view = view;
     }
 
+    /** 插入view到指定位置 */
+    public insertTo(stageView: any) {
+        const views = stageView.subviews();
+        const finalViews = [];
+        let pushedWebView = false;
+        for (let i = 0; i < views.count(); i++) {
+            const view = views[i];
+            finalViews.push(view);
+            if (!pushedWebView && ''.concat(view.identifier()) === 'view_canvas') {
+                finalViews.push(this.view);
+                pushedWebView = true;
+            }
+        }
+        NSUserDefaults.standardUserDefaults().setObject_forKey('true', this.id);
+        stageView.subviews = finalViews;
+        stageView.adjustSubviews();
+    }
 
+    /** 移除view */
+    public removeFrom(stageView: any) {
+        const views = stageView.subviews();
+        const finalViews = [];
+        for (let i = 0; i < views.count(); i++) {
+            const view = views[i];
+            if (''.concat(view.identifier()) !== this.id) {
+                finalViews.push(view);
+            }
+        }
+        NSUserDefaults.standardUserDefaults().setObject_forKey(nil, this.id);
+        stageView.subviews = finalViews;
+        stageView.adjustSubviews();
+    }
+
+
+    public shutdown(documentID: string){
+        const threadDictionary = NSThread.mainThread().threadDictionary();
+        //  BrowserManage.empty()
+        const prefixRegExp = new RegExp(`${this.prefix}*`)
+        const webViewPrefixRegExp = new RegExp(`${this.prefix}-webview*`)
+
+        for (const key in threadDictionary) {
+          if (prefixRegExp.test(key)) {
+            if (webViewPrefixRegExp.test(key)) {
+              threadDictionary[key].close()
+            }
+            threadDictionary.removeObjectForKey(key)
+          }
+        }
+
+        // clear WindowResizeNotification
+        this.removeObserverWindowResizeNotification(documentID);
+        COScript.currentCOScript().setShouldKeepAround(false);
+    }
+    private removeObserverWindowResizeNotification(documentID: string) {
+        const delegateInstance = NSThread.mainThread().threadDictionary()(`window-move-instance-${documentID}`)
+        if (delegateInstance) {
+          NSNotificationCenter.defaultCenter().removeObserver_name_object(delegateInstance, NSWindowDidResizeNotification, nil)
+          removeThreadDictForKey(`window-move-instance-${documentID}`)
+          removeThreadDictForKey(`window-move-selector-${documentID}`)
+        }
+    }
 }
 
-export interface InjectablePanel {
-    // static getIDPrefix(documentID?: string) : string;
-
+function removeThreadDictForKey(key: any) {
+    if (NSThread.mainThread().threadDictionary()[key]) NSThread.mainThread().threadDictionary().removeObjectForKey(key)
 }
